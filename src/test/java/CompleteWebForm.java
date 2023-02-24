41de11ff84e352b0cbfedaa594c8097b4057c5e0
Ivan Adzic <121823205+ivanadzic@users.noreply.github.com>
2023-02-25 00:34:25 +0100

    Update CompleteWebForm.java

--- src/test/java/CompleteWebForm.java
+++ src/test/java/CompleteWebForm.java
@@ -1,15 +1,8 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import pages.ConfirmationPage;
 import pages.FormPage;
-
-import java.time.Duration;
 
 public class CompleteWebForm {
   public static void main(String[] args) {
