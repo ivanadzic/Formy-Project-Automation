f1d70f29f6026dc2c378dd959e6d08ca0efe93b9
Ivan Adzic <121823205+ivanadzic@users.noreply.github.com>
2023-02-25 00:29:37 +0100

    Update CompleteWebForm.java

--- src/test/java/CompleteWebForm.java
+++ src/test/java/CompleteWebForm.java
@@ -23,56 +23,12 @@ public class CompleteWebForm {
     FormPage formPage = new FormPage();
     formPage.submitForm(driver);
 
-//    submitForm(driver);
-
     ConfirmationPage confirmationPage = new ConfirmationPage();
     confirmationPage.waitForAlertBanner(driver);
-
-//    waitForAlertBanner(driver);
 
     Assert.assertEquals("The form was successfully submitted!", confirmationPage.getAlertBannerText(driver));
 
     driver.quit();
 
   }
-
-//  public static void submitForm(WebDriver driver) {
-//    WebElement firstNameField = driver.findElement(By.id("first-name"));
-//    firstNameField.sendKeys("John");
-//
-//    WebElement lastNameField = driver.findElement(By.id("last-name"));
-//    lastNameField.sendKeys("Doe");
-//
-//    WebElement jobTitle = driver.findElement(By.id("job-title"));
-//    jobTitle.sendKeys("QA Specialist");
-//
-//    WebElement educationButton = driver.findElement(By.id("radio-button-1"));
-//    educationButton.click();
-//
-//    WebElement sexCheckBox = driver.findElement(By.id("checkbox-1"));
-//    sexCheckBox.click();
-//
-//    WebElement experienceDropdown = driver.findElement(By.id("select-menu"));
-//    experienceDropdown.click();
-//
-//    WebElement experiencetValue = driver.findElement(By.cssSelector("option[value='1']"));
-//    experiencetValue.click();
-//
-//    WebElement datePicker = driver.findElement(By.id("datepicker"));
-//    datePicker.sendKeys("01/01/2022");
-//
-//    datePicker.sendKeys(Keys.RETURN);
-//
-//    WebElement submitButton = driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary"));
-//    submitButton.click();
-//  }
-//
-//  public static void waitForAlertBanner(WebDriver driver) {
-//    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//    wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
-//  }
-//
-//  public static String getAlertBannerText(WebDriver driver) {
-//    return driver.findElement(By.className("alert")).getText();
-//  }
 }
